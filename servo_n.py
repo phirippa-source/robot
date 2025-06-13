38859fbcc15b2d0ea752a5ffdf79108f2f5de0bc
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-06-13 10:45:01 +0900

    Update servo_n.py

--- servo_n.py
+++ servo_n.py
@@ -1,14 +1,18 @@
-# adafruit_pca9685 모듈 설치
-# bullseye인 경우, $ pip install adafruit_pca9685
-
 import Adafruit_PCA9685
 import time
 
 pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(50)  # 50Hz
 
-for _ in range(3):
-  pwm.set_pwm(0,0,300)
-  time.sleep(1)
-  pwm.set_pwm(0,0,400)
-  time.sleep(1)
+try:
+  for _ in range(3):
+    pwm.set_pwm(0,0,300)
+    pwm.set_pwm(1,0,120)    # min : about 100
+    time.sleep(2)
+
+    pwm.set_pwm(0,0,400)
+    pwm.set_pwm(1,0,540)    # max : about 560
+    time.sleep(2)
+except KeyboardInterrupt:
+  pwm.set_pwm(0,0,120)
+  pwm.set_pwm(2,0,120)
