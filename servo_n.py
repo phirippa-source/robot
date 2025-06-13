35c4ce4ce4c878a1cc31bade8422375106ef1789
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-06-13 10:40:45 +0900

    Create servo_n.py

---
+++ servo_n.py
@@ -0,0 +1,14 @@
+# adafruit_pca9685 모듈 설치
+# bullseye인 경우, $ pip install adafruit_pca9685
+
+import Adafruit_PCA9685
+import time
+
+pwm = Adafruit_PCA9685.PCA9685()
+pwm.set_pwm_freq(50)  # 50Hz
+
+for _ in range(3):
+  pwm.set_pwm(0,0,300)
+  time.sleep(1)
+  pwm.set_pwm(0,0,400)
+  time.sleep(1)
